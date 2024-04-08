0c3f14fa6b79ea89ea6e51ead44394e19369386c
Ishaan Desai <ishaandesai@gmail.com>
2024-04-08 15:14:25 +0200

    Bump version

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % MATLAB-bindings for coupling library preCICE
-% Version 3.0.0.0 (R2023b, R2023a, R2022b, R2022a, R2021b) 05-Feb-2024
+% Version 3.1.0.0 (R2023b, R2023a, R2022b, R2022a, R2021b) 08-Apr-2024
 %
 % Files
 %   getVersionInformation - 
