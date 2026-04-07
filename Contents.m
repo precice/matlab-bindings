c6b62d35dc1364d35690cfb244ec8fcc54805ed3
Ishaan Desai <ishaandesai@gmail.com>
2026-04-07 18:10:19 +0200

    Add R2025a to supported MATLAB versions

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % MATLAB-bindings for coupling library preCICE
-% Version 3.3.0 (R2024a, R2023b) 09-Oct-2025
+% Version 3.3.0 (R2025a, R2024a, R2023b) 07-Apr-2026
 %
 % Files
 %   getVersionInformation - 
